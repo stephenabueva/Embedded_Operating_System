67dccad72bab97e5f094d0c3a48e03b0824c6b97
stephenabueva <92534123+stephenabueva@users.noreply.github.com>
2022-11-02 13:28:52 +0800

    Update first_in_first_out.c

--- first_in_first_out.c
+++ first_in_first_out.c
@@ -1,6 +1,32 @@
-int green=5;
-int yellow=6;
-int orange=7;
-int blue=8;
-int red=9;
-int white=10;
+void setup()
+{
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+   
+}
+void loop()
+{
+  digitalWrite(5, HIGH);
+  delay(5000); // Wait for 5000 millisecond(s)
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  delay(8000); // Wait for 8000 millisecond(s)
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  delay(7000); // Wait for 7000 millisecond(s)
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  delay(6000); // Wait for 6000 millisecond(s)
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  delay(4000); // Wait for 4000 millisecond(s)
+  digitalWrite(10, LOW);
+  while(1){};
+}
