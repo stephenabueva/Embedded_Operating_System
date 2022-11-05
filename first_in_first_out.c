e9607619cfec799991cb710a09e55533942a6944
stephenabueva <92534123+stephenabueva@users.noreply.github.com>
2022-11-05 20:54:41 +0800

    Update first_in_first_out.c

--- first_in_first_out.c
+++ first_in_first_out.c
@@ -1,32 +1,35 @@
-void setup()
-{
+/*the arduino sketch below visualizes the first in
+first out algorithm, the first LED will be light up first
+and the succeeding operation will be performed until all the task is finised
+*/
+
+void setup(){
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
-   
 }
-void loop()
-{
-  digitalWrite(5, HIGH);
-  delay(5000); // Wait for 5000 millisecond(s)
-  digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);
-  delay(3000); // Wait for 3000 millisecond(s)
-  digitalWrite(6, LOW);
-  digitalWrite(7, HIGH);
-  delay(8000); // Wait for 8000 millisecond(s)
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
-  delay(7000); // Wait for 7000 millisecond(s)
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
-  delay(6000); // Wait for 6000 millisecond(s)
-  digitalWrite(9, LOW);
-  digitalWrite(10, HIGH);
-  delay(4000); // Wait for 4000 millisecond(s)
-  digitalWrite(10, LOW);
-  while(1){};
+
+void loop(){
+  digitalWrite(5,HIGH);
+  delay(10000);
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  delay(8000);
+  digitalWrite(6,LOW);
+  digitalWrite(7,HIGH);
+  delay(4000);
+  digitalWrite(7,LOW);
+  digitalWrite(8,HIGH);
+  delay(2000);
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+  delay(6000);
+  digitalWrite(9,LOW);
+  digitalWrite(10,HIGH);
+  delay(4500);
+  digitalWrite(10,LOW);
+  while(1);
 }
